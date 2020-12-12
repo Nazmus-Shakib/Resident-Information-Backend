69daeb7985a5c4b2c9ae04f8f88778ed7f011492
Nazmus-Shakib <shakibnazmus16@gmail.com>
2020-12-13 02:59:58 +0800

    CRUD added

--- index.js
+++ index.js
@@ -47,6 +47,27 @@ client.connect((err) => {
       });
   });
 
+  // to update residents to database
+  app.patch("/updateResident/:id", (req, res) => {
+    console.log(req.body.name);
+
+    residentsCollection
+      .updateOne(
+        { _id: ObjectId(req.params.id) },
+        {
+          $set: {
+            name: req.body.name,
+            email: req.body.email,
+            address: req.body.address,
+            phone: req.body.phone,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      });
+  });
+
   // to delete specific resident from database
   app.delete("/deleteResident/:id", (req, res) => {
     //console.log(req.params.id);
@@ -54,6 +75,7 @@ client.connect((err) => {
       .deleteOne({ _id: ObjectId(req.params.id) })
       .then((result) => {
         console.log(result);
+        //res.send(result.deletedCount > 0);
       });
   });
 
